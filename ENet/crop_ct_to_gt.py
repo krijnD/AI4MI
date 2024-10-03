57bbc8eaaa56a1058012ccbed861c33d7da15083
MellevdB <mellevdbrugge@gmail.com>
2024-10-03 20:03:12 +0200

    Refactor crop_ct_to_gt.py to only crop along the z-axis

--- ENet/crop_ct_to_gt.py
+++ ENet/crop_ct_to_gt.py
@@ -3,7 +3,7 @@ import nibabel as nib
 import numpy as np
 
 # Define paths to the data
-base_folder = '../data/segthor_train/train'
+base_folder = 'data/segthor_train/train'
 patient_folders = [f'Patient_{i:02d}' for i in range(1, 41)]
 
 for patient in patient_folders:
@@ -20,18 +20,14 @@ for patient in patient_folders:
         ct_data = ct_img.get_fdata()
         gt_data = gt_img.get_fdata()
         
-        # Find non-zero slices in the GT segmentation along the x, y, and z axes
-        non_zero_x = np.where(np.any(gt_data, axis=(1, 2)))[0]
-        non_zero_y = np.where(np.any(gt_data, axis=(0, 2)))[0]
-        non_zero_z = np.where(np.any(gt_data, axis=(0, 1)))[0]
+        # Find non-zero slices in the GT segmentation along the z-axis (depth)
+        non_zero_slices = np.where(np.any(gt_data, axis=(0, 1)))[0]
         
-        # Get the bounding box for non-zero values
-        x_min, x_max = non_zero_x[0], non_zero_x[-1]
-        y_min, y_max = non_zero_y[0], non_zero_y[-1]
-        z_min, z_max = non_zero_z[0], non_zero_z[-1]
+        # Get the first and last non-zero slice index from the GT data
+        z_min, z_max = non_zero_slices[0], non_zero_slices[-1]
         
-        # Crop the CT scan to match the GT bounding box
-        cropped_ct_data = ct_data[x_min:x_max + 1, y_min:y_max + 1, z_min:z_max + 1]
+        # Crop the CT scan to match the GT range along the z-axis
+        cropped_ct_data = ct_data[:, :, z_min:z_max + 1]
         
         # Create a new NIfTI image for the cropped CT scan
         cropped_ct_img = nib.Nifti1Image(cropped_ct_data, affine=ct_img.affine)
