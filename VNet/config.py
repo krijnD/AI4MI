a840d808972bc9089b76d36483f3259c40dfab2d
krijnD <krijnfelipedignum@gmail.com>
2024-10-24 22:35:55 +0200

    delete VNet

--- VNet/config.py
+++
@@ -1,7 +0,0 @@
-from typing import Any
-
-class params:
-
-	datasets_params: dict[str, dict[str, Any]] = {}
-	datasets_params["TOY2"] = {'K': 2, 'net': None, 'B': 2}
-	datasets_params["SEGTHOR"] = {'K': 5, 'net': None, 'B': 8}
